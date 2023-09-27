2d11bf7854e4c3223dd4bd66f4817367315f307e
matvenikus <maatvenikus@gmail.com>
2023-09-27 23:09:19 +0300

    add findMin method

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin () {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 0, 8, 4, 5, 3, 8, 6, 11, 11, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
